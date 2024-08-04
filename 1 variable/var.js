a1005ae50b7cd52e5fa6598fe9d8e83d201cdfd3
logeshp256 <logeshkumar2563@psnacet.edu.in>
2024-08-04 10:19:42 +0530

    Add files via upload

---
+++ 1 variable/var.js
@@ -0,0 +1,8 @@
+let name = "hello ";
+let age = 21;
+let tre = true;
+
+document.getElementById('p1').innerHTML =`your name is ${name}`
+document.getElementById('p2').innerHTML =`your name is ${age}`
+document.getElementById('p3').textContent =`your name is ${tre}`
+
---
+++ 2 operators/op.js
@@ -0,0 +1,10 @@
+let students=30;
+
+
+students += 1;
+students -= 1;
+students *= 2;
+students /= 2;
+students **= 2;
+students %= 2;
+console.log(students)
